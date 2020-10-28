6d48087c871c1be21205b28c7b1caa6aa19a1ac2
MercyMueniMwangi <mercymwangi1996@gmail.com>
2020-10-28 19:03:53 +0000

    created _printf file, gitignore file and _putchar file

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *_printf: A function that mimics the fuctions of a printf() function in c
+ *@format:list of argument types passed to the function
+ *
+ *Return: Characters printed
+ */
+
+
+int _printf(const char *, ...)
+{
+	
