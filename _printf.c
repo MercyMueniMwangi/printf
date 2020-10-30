e93d9984b5f3a687343d8ec0c3ee1beff7e6f4fd
MercyMueniMwangi <mercymwangi1996@gmail.com>
2020-10-30 18:55:49 +0000

    Found the bug it was un used *ch

--- _printf.c
+++ _printf.c
@@ -1,32 +1,51 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
 #include "holberton.h"
-/**
- * _printf - print a char or a string
- * @format: It's a character string
- * Return: the number of character the function is printing
- */
 int _printf(const char *format, ...)
 {
-	st_fmt st_format[] = {
-		{"c", func_char},
-		{"s", func_string},
-		{"d", func_digit},
-		{"i", func_digit},
-		{"%", func_percent},
-		{"b", func_binary_convert},
-		{"u", func_unsig_int},
-		{"o", func_octal_convert},
-		{"x", func_hex_Lowcase_convert},
-		{"X", func_hex_Upcase_convert},
-		{"S", func_stringUppercase},
-		{"r", func_revstr},
-		{"R", func_rot13},
-		{NULL, NULL}};
+	unsigned int count = 0;
+	char *str;
+	/*char *ch;*/
+	va_list args;
 
-	va_list printlist;
-	int count = 0;
-
-	va_start(printlist, format);
-	count =get_match_func(format, printlist, st_format);
-	va_end(printlist);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+			case 's':
+				str = va_arg(args, char *);
+				fputs(str, stdout);
+				format++;
+				count += strlen(str);
+				break;
+			case '%':
+				putchar('%');
+				format++;
+				count++;
+				break;
+			case 'c':
+				count = va_arg(args, int);
+				putchar(count);
+				break;
+			default:
+				putchar('%');
+				putchar(*format);
+				format++;
+				count += 2;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			format++;
+			count++;
+		}
+	}
+	va_end(args);
 	return (count);
 }
